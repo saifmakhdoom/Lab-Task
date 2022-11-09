3b948ee42381cf201a79e773d8284eb089139225
saifmakhdoom <117858367+saifmakhdoom@users.noreply.github.com>
2022-11-09 11:21:31 -0800

    Add files via upload

---
+++ labtast1.java
@@ -0,0 +1,15 @@
+ public class labtast1 {
+    public static void main(String[] args) {
+        int mySetNo = 41;              
+        float myNumber = 0315.55f;    
+        char myLetter = 'S';         
+        boolean mybool = true;       
+        String myText = "My Name Is Saif ur rehman  from sukkur
+        System.out.println(mySetNo);
+        System.out.println(myNumber);
+        System.out.println(myLetter);
+        System.out.println(mybool);
+        System.out.println(myText);
+    }
+}
+
